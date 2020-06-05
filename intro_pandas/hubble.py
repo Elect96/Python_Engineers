44942953f00c07a538472cb6a18bc3210734c2bd
Jakub Lamczyk <lamczykjakub@gmail.com>
2020-06-05 10:41:57 +0100

    Image and video processing next

---
+++ intro_pandas/hubble.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python
+# https://www.pythonforengineers.com/introduction-to-pandas/import pandas as pd
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("hubble_data.csv")
+data.head()
+data.set_index("distance", inplace=True)
+data.plot()
+plt.show()
+
+# no headers .csv handling
+# headers = ["dist", "rec_vel"]
+# data_no_headers = pd.read_csv("hubble_data_no_headers.csv", names=headers)
+# data_no_headers.set_index("dist", inplace=True)
+# data_no_headers.plot()
+# plt.show()
+
---
+++ intro_pandas/wages.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python
+# https://www.pythonforengineers.com/introduction-to-pandas/import pandas as pd
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+data = pd.read_csv("wages_hours.csv", sep="\t")
+data2 = data[["AGE", "RATE"]]
+data_sorted = data2.sort_values(["AGE"])
+data_sorted.set_index("AGE", inplace=True)
+
+data_sorted.plot()
+plt.show()

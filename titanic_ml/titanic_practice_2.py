3996c7541f49fad3acda612aaa90b5c0affa8787
Jakub Lamczyk <lamczykjakub@gmail.com>
2020-06-14 10:21:29 +0100

    start Cross Validation and Model Selection

--- titanic_ml/titanic_practice_2.py
+++ titanic_ml/titanic_practice_2.py
@@ -2,17 +2,22 @@
 # src: https://www.pythonforengineers.com/machine-learning-for-complete-beginners/
 import numpy as np
 import pandas as pd
-# machine learning algorithm
-from sklearn.ensemble import RandomForestClassifier
-# test train split
-from sklearn.model_selection import train_test_split
 # write the model to a file
 from sklearn.externals import joblib
 # switch off pandas warning
 pd.options.mode.chained_assignment = None
 
+
+def find_error(prediction):
+    titanic_data = np.loadtxt("titanic_results.txt", dtype="int32")
+    diff = np.equal(titanic_data, prediction)
+    correct_answers = np.sum(diff)
+    percent_diff = correct_answers / len(prediction) * 100
+    print("Titanic: Percentage Match is: ", percent_diff)
+
+
 # data read
-data = pd.read_csv("titanic_train.csv")
+data = pd.read_csv("titanic_test.csv")
 
 # data clean-up
 data_input = data[['pclass', 'sex']]
@@ -21,18 +26,11 @@ data_input.replace("2nd", 2, inplace=True)
 data_input.replace("1st", 1, inplace=True)
 data_input['sex'] = np.where(data_input['sex'] == "female", 0, 1)
 
-# outcome
-expected_outcome = data[['survived']]
-input_train, input_test, expected_output_train, expected_output_test = \
-    train_test_split(data_input, expected_output, test_size=.33, random_state=42)
 # load the model
 rf = joblib.load("titanic_model_2")
 
 # make a prediction
 pred = rf.predict(data_input)
 
-# calculate accuracy
-accuracy = rf.score(data_input, pred)
-print("Accuracy: {}%".format(accuracy * 100))
-
-# sort out the outcome, it shows 100% accuracy which is for sure wrong
+# model accuracy
+find_error(pred)
